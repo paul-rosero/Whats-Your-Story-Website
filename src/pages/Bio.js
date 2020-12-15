ccae0f051f8349b2860305bd072b560fdb3309b2
Paul Rosero <paul.rosero23@gmail.com>
2020-12-15 14:41:45 -0500

    create a bio file to put all the info for the bio

---
+++ src/pages/Bio.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Bio extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Marisol's Bio Page</h2>
+            </div>
+        )
+    }
+}
+
+export default Bio;
