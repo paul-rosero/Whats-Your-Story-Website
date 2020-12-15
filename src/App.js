8b88f8e53685d4ce56531530199263a4ecf4563c
Paul Rosero <paul.rosero23@gmail.com>
2020-12-15 16:09:43 -0500

    add NavBar component so it renders

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import { Container, Col, Row, Nav } from 'react-bootstrap';
-
 import './App.css';
+import NavBar from './components/NavBar';
 import  Routes  from './components/Routes';
 
 
 function App() {
   return (
     <div>
-      <header>
-        <Nav defaultActiveKey="/home" >
-          <Nav.Item >
-            <Nav.Link href="/">Homepage</Nav.Link>
-          </Nav.Item>
-          <Nav.Item >
-            <Nav.Link href="/bio" eventKey="Bio-link">Bio</Nav.Link>
-          </Nav.Item>
-          <Nav.Item >
-            <Nav.Link href="/my-work" eventKey="My-Work-link">My work</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </header>
-      <body className="container">
-        <Routes />
-      </body>
+      <NavBar />
+      <Routes />
+      <footer>
+        <p>this page was created by Paul Rosero for Whats your Story</p>
+      </footer>
     </div>
   );
 }
