77578320e823b56c92ca1c357dd50b304280c4b5
Paul Rosero <paul.rosero23@gmail.com>
2020-12-15 14:39:31 -0500

    add navbar and body to webpage with working links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { Container, Col, Row, Nav } from 'react-bootstrap';
+
 import './App.css';
+import  Routes  from './components/Routes';
+
 
 function App() {
   return (
     <div>
-      <h1>Marisol's Page</h1>
+      <header>
+        <Nav defaultActiveKey="/home" >
+          <Nav.Item >
+            <Nav.Link href="/">Homepage</Nav.Link>
+          </Nav.Item>
+          <Nav.Item >
+            <Nav.Link href="/bio" eventKey="Bio-link">Bio</Nav.Link>
+          </Nav.Item>
+          <Nav.Item >
+            <Nav.Link href="/my-work" eventKey="My-Work-link">My work</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </header>
+      <body className="container">
+        <Routes />
+      </body>
     </div>
   );
 }
