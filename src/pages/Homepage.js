a5ff893df41f53e329fa4c883b2bb2d3345a8272
Paul Rosero <paul.rosero23@gmail.com>
2020-12-15 14:42:50 -0500

    add file to be able to have a landing page

---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Homepage extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Marisol's Homepage</h2>
+            </div>
+        )
+    }
+}
+
+export default Homepage;
