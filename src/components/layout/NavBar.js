fa40c98333fa3aea01e60a5b743e09c07ccab175
Paul Rosero <paul.rosero23@gmail.com>
2020-12-16 16:17:39 -0500

    changed the way i called Row, Col and responsive navbar

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,27 +1,34 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, Col, Row } from 'react-bootstrap';
 
 function NavBar() {
     return (
         <header>
             <Navbar collapseOnSelect expand="md">
-                <Navbar.Brand href="/" style={{color: "white"}}>
-                    <img
-                        src="/whatsyourstory-icon.jpg"
-                        width="70"
-                        height="70"
-                        className="d-inline-block align-top"
-                        alt=""
-                    />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
+                <Row className="navbar-brand" md={ 2 } >
+                    <Col >
+                        <Navbar.Brand href="/" style={{ color: "white", fontWeight: "bold", fontSize: "1.2em", textAlign: "center" }}>
+                            Marisol Pena
+                            <p style={{ fontSize: "small"}}>Story Teller</p>
+                        </Navbar.Brand>
+                    
+                        
+                    </Col>
+                </Row>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto navbar-links">
                         <Nav.Link href="/" style={{color: "white"}}>Home</Nav.Link>
                         <Nav.Link href="/my-story" style={{color: "white"}}>My Story</Nav.Link>
                         <Nav.Link href="/merch" style={{color: "white"}}>Merch</Nav.Link>
                         <Nav.Link href="/podcast" style={{color: "white"}}>Podcast</Nav.Link>
                         <Nav.Link href="/contact" style={{color: "white"}}>Contact</Nav.Link>
+                    </Nav>
+                    <Nav className="navbar-icons">
+                        <Nav.Link href="" style={{color: "white"}}><i className="fab fa-facebook"/></Nav.Link>
+                        <Nav.Link href="" style={{color: "white"}}><i className="fab fa-instagram-square"/></Nav.Link>
+                        <Nav.Link href="" style={{color: "white"}}><i className="fab fa-twitter-square"/></Nav.Link>
+                        <Nav.Link href="" style={{color: "white"}}><i className="fab fa-youtube-square"/></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
