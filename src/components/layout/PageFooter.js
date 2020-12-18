30d88c2b48dbbe8e1dfb24a2a678bb00d24f2c2c
Paul Rosero <paul.rosero23@gmail.com>
2020-12-17 19:02:56 -0500

    add p tag saying this page was created by me

--- src/components/layout/PageFooter.js
+++ src/components/layout/PageFooter.js
@@ -4,15 +4,14 @@ import { Footer, FooterSection, FooterLinkList } from "react-mdl";
 
 function PageFooter() {
     return (
-        <Footer size="mini" style={{ background: "rgb(40,43,130)" }}>
+        <Footer size="mini" style={{ background: "rgb(40,43,130)", color: "white" }}>
             <FooterSection type="left" logo="logo">
-                <FooterLinkList>
-                    <a href="/">Help</a>
+                <FooterLinkList>  
                     <a href="/">Privacy & Terms</a>
+                    <p>This website was created By Paul Rosero.</p>
                 </FooterLinkList>
             </FooterSection>
         </Footer>
-        
     )
 }
 
