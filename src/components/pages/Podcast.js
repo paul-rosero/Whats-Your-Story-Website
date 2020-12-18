24a44802855aaeeaf3923458ebfbbe0d165dffb1
Paul Rosero <paul.rosero23@gmail.com>
2020-12-18 11:09:13 -0500

    create Podcast class component content

---
+++ src/components/pages/Podcast.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Podcast extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h2>Marisol's Podcast Page</h2>
+            </div>
+        )
+    }
+}
+
+export default Podcast;
