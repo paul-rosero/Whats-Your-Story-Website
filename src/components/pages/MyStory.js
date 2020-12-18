9f427d7a11bb155a166c2e881c2f6b09f8a06b78
Paul Rosero <paul.rosero23@gmail.com>
2020-12-18 11:44:45 -0500

    import whatsyourstory icon and display it in second col tag

--- src/components/pages/MyStory.js
+++ src/components/pages/MyStory.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import whatsYourStory from "../../images/whatsyourstory-icon.jpg";
 
 class MyStory extends Component {
     render() {
         return (
             <div className="container">
-                <h2>Marisol's MyStory Page</h2>
+                <Row>
+                    <Col style={{ backgroundColor: "rgb(236,108,107)" }}>
+                        <h2>My Story</h2>
+                        <h2>Marisol's MyStory Page</h2>
+                    </Col>
+                    <Col style={{ backgroundColor: "white", padding: "30px 10px" }}>
+                        <img
+                            src={ whatsYourStory }
+                            alt="whats-your-story-icon"
+                            fluid
+                            width="100%"
+                            height="auto"
+                        ></img>
+                    </Col>
+                </Row>
             </div>
         )
     }
