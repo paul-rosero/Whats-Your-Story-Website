2b3d29ca2e373583a958a043a9be7bd1d3848e40
Paul Rosero <paul.rosero23@gmail.com>
2020-12-18 11:07:30 -0500

    add the rest of route components and import them

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
-import MyStory from './pages/MyStory'
-import Homepage from './pages/Homepage'
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import MyStory from './pages/MyStory';
+import Homepage from './pages/Homepage';
+import Merch from './pages/Merch';
+import Podcast from './pages/Podcast';
+import Contact from './pages/Contact';
 
 function Routes() {
     return (
         <Switch>
             <Route exact path="/" component={ Homepage } />
             <Route path="/my-story" component={ MyStory } />
-            <Route path="/my-story" component={  } />
-            <Route path="/my-story" component={ MyStory } />
-            <Route path="/my-story" component={ MyStory } />
+            <Route path="/merch" component={ Merch } />
+            <Route path="/podcast" component={ Podcast } />
+            <Route path="/contact" component={ Contact } />
         </Switch>
     )
 }
