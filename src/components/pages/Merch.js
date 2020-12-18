c1e067282c6b0b6ea89ad22c8b3d03b977841224
Paul Rosero <paul.rosero23@gmail.com>
2020-12-18 11:08:43 -0500

    create Merch class component content

---
+++ src/components/pages/Merch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Merch extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h2>Marisol's Merch Page</h2>
+            </div>
+        )
+    }
+}
+
+export default Merch;
