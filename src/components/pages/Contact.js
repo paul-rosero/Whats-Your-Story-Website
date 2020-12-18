62def9a833a72b67b1c086bc9fe5f147181c8a7a
Paul Rosero <paul.rosero23@gmail.com>
2020-12-18 11:08:25 -0500

    create Contact class component

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h2>Marisol's Contact Page</h2>
+            </div>
+        )
+    }
+}
+
+export default Contact;
