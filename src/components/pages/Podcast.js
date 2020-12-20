a0c04b43c5c067f1cfc876fd1b29848b9343ea63
Paul Rosero <paul.rosero23@gmail.com>
2020-12-20 16:23:17 -0500

    add iframe tag where the podcast player is placed

--- src/components/pages/Podcast.js
+++ src/components/pages/Podcast.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 class Podcast extends Component {
     render() {
         return (
-            <div className="container">
+            <div>
                 <h2>Marisol's Podcast Page</h2>
+                <iframe title="Podcast" id="player_iframe" src="https://www.buzzsprout.com/1082117?client_source=large_player&amp;amp;iframe=true" scrolling="no" width="100%" height="370" frameBorder="2"></iframe>
             </div>
         )
     }
