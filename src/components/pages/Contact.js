90cbcb30c4651ad4e5fe55c3b58473b370ec3436
Paul Rosero <paul.rosero23@gmail.com>
2020-12-27 20:46:27 -0500

    refactor code so that I map all the pics instead of writing indivudually

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,21 +4,25 @@ import marisol2 from "../../images/marisol2.jpeg";
 import whatsYourStory from "../../images/whatsyourstory-icon.jpg";
 import editingPodcast from "../../images/editing_podcast.jpeg";
 
+const allImages = [
+    { id: 1, src: marisol2, alt: "marisol2" },
+    { id: 2, src: whatsYourStory, alt: "whatsyourstory-icon" },
+    { id: 3, src: editingPodcast, alt: "editing_podcast" }
+]
+
+const RenderImages = allImages.map( ({ id, src, alt }) => {
+    return <Col key={ id } xs={ 6 } md={ 4 }>
+        <Image style={{ objectFit: "cover", bottom: "auto", margin: "0px", height: "300px", width: "100%", borderRadius: "0px", borderWidth: "2px", borderColor: "rgb(40, 43, 130)", borderStyle: "solid", transition: "none 0s ease 0s"}} src={ src } alt={ alt } fluid />
+    </Col>
+})
+
 class Contact extends Component {
     render() {
         return (
             <div className="container">
                 <Row style={{ padding: "10px 0px"}} className="contact-images"> 
-                    <Col xs={6} md={4}>
-                        <Image style={{ bottom: "auto", margin: "0px", height: "auto", width: "100%", borderRadius: "0px", borderWidth: "2px", borderColor: "rgb(40, 43, 130)", borderStyle: "solid", transition: "none 0s ease 0s"}} src={ whatsYourStory } alt="whatsyourstory-image" fluid />
-                    </Col>
-                    <Col xs={6} md={4}>
-                        <Image style={{ bottom: "auto", margin: "0px", height: "auto", width: "100%", borderRadius: "0px", borderWidth: "2px", borderColor: "rgb(40, 43, 130)", borderStyle: "solid", transition: "none 0s ease 0s"}} src={ editingPodcast } alt="editing-podcast-image" fluid />
-                    </Col>    
-                    <Col xs={6} md={4}>
-                        <Image style={{ bottom: "auto", margin: "0px", height: "auto", width: "100%", borderRadius: "0px", borderWidth: "2px", borderColor: "rgb(40, 43, 130)", borderStyle: "solid", transition: "none 0s ease 0s"}} src={ marisol2 } alt="marisol2-image" fluid/>
-                    </Col>
-                </Row>
+                    { RenderImages }
+                </Row> 
                 <Row style={{ backgroundColor: "rgb(40,43,130)" }}>
                     <Col xs={ 8 } md={ 9 }>
                         <h3>hello1</h3>
