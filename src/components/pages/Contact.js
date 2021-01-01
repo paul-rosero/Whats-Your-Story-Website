977904578fc3049232db7896b22bf659f11df5dd
Paul Rosero <paul.rosero23@gmail.com>
2021-01-01 12:51:55 -0500

    add podcastEquipment pic to array

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,18 +3,19 @@ import { Col, Image, Row } from 'react-bootstrap';
 import marisol2 from "../../images/marisol2.jpeg";
 import whatsYourStory from "../../images/whatsyourstory-icon.jpg";
 import editingPodcast from "../../images/editing_podcast.jpeg";
+import podcastEquipment from "../../images/podcast_equipment.jpg"
 import { EmailForm } from '../Forms'; 
 
 
 const allImages = [
-    { id: 1, src: marisol2, alt: "marisol2" },
-    { id: 2, src: whatsYourStory, alt: "whatsyourstory-icon" },
-    { id: 3, src: editingPodcast, alt: "editing_podcast" }
-
+    { src: whatsYourStory, alt: "whatsyourstory-icon" },
+    { src: editingPodcast, alt: "editing_podcast" },
+    { src: podcastEquipment, alt: "podcast-equipment" },
+    { src: marisol2, alt: "marisol2" }
 ]
 
-const RenderImages = allImages.map( ({ id, src, alt }) => {
-    return <Col key={ id } >
+const RenderImages = allImages.map( ({ src, alt }, index) => {
+    return <Col key={ index } >
         <Image style={{ objectFit: "cover", margin: "0", height: "100%", width: "100%", borderRadius: "0px", borderWidth: "2px", borderColor: "rgb(40, 43, 130)", borderStyle: "solid", transition: "none 0s ease 0s"}} src={ src } alt={ alt } fluid />
     </Col>
 })
