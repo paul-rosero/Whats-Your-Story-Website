0d39b0a7853e6255af127758faa7fcb570337fc5
Paul Rosero <paul.rosero23@gmail.com>
2021-01-01 12:42:40 -0500

    remove subscribeEmail since its no longer wanted

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,18 +3,19 @@ import { Col, Image, Row } from 'react-bootstrap';
 import marisol2 from "../../images/marisol2.jpeg";
 import whatsYourStory from "../../images/whatsyourstory-icon.jpg";
 import editingPodcast from "../../images/editing_podcast.jpeg";
-import { EmailForm, SubscribeForm } from '../Forms'; 
+import { EmailForm } from '../Forms'; 
 
 
 const allImages = [
     { id: 1, src: marisol2, alt: "marisol2" },
     { id: 2, src: whatsYourStory, alt: "whatsyourstory-icon" },
     { id: 3, src: editingPodcast, alt: "editing_podcast" }
+
 ]
 
 const RenderImages = allImages.map( ({ id, src, alt }) => {
-    return <Col key={ id } xs={ 8 } md={ 4 }>
-        <Image style={{ objectFit: "cover", bottom: "auto", margin: "0px", height: "100%", width: "100%", borderRadius: "0px", borderWidth: "2px", borderColor: "rgb(40, 43, 130)", borderStyle: "solid", transition: "none 0s ease 0s"}} src={ src } alt={ alt } fluid />
+    return <Col key={ id } >
+        <Image style={{ objectFit: "cover", margin: "0", height: "100%", width: "100%", borderRadius: "0px", borderWidth: "2px", borderColor: "rgb(40, 43, 130)", borderStyle: "solid", transition: "none 0s ease 0s"}} src={ src } alt={ alt } fluid />
     </Col>
 })
 
@@ -26,12 +27,11 @@ class Contact extends Component {
                     { RenderImages }
                 </Row> 
                 <Row style={{ backgroundColor: "rgb(40,43,130)" }}>
-                    <Col xs={ 8 } md={ 6 }>
-                        <EmailForm/>
+                    <Col >
+                        
                     </Col>
-                    <Col xs={ 4 } md={ 6 }>
-                    <h4>Weekly Updates</h4>
-                       <SubscribeForm/>
+                    <Col>
+                        <EmailForm/>
                     </Col>
                 </Row>
             </div>
