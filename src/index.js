64c2b061867a729a70b33484db3cb54e2608b453
Paul Rosero <paul.rosero23@gmail.com>
2021-01-02 13:45:51 -0500

    remove  import react-mdl since im not using it

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-import 'react-mdl/extra/material.css';
-import 'react-mdl/extra/material.js';
 
 ReactDOM.render(
   <BrowserRouter>
