4148bac0ba46be28f19ac4f0ac1182ecfccaa6ab
Paul Rosero <paul.rosero23@gmail.com>
2021-01-02 13:33:14 -0500

    added code to create footer page

--- src/components/layout/PageFooter.js
+++ src/components/layout/PageFooter.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import { Container, Row } from 'react-bootstrap';
 
 function PageFooter() {
     return (
-        
-        // style={{ contentAlign: "center", background: "rgb(40,43,130)", color: "white" }}
-        //      <p>This website was created By <a href="https://paul-rosero-portfolio.herokuapp.com/" target="_blank" rel="noopener noreferrer">Paul Rosero</a>.</p>
+        <Container>
+            <Row style={{ contentAlign: "center", background: "rgb(40,43,130)", color: "white", padding: "1vmax", justifyContent: "center" }}>
+                <p>&copy; {new Date().getFullYear()} Copyright: Created by <a style={{ fontSize: "2.5vh" }} href="https://paul-rosero-portfolio.herokuapp.com/" target="_blank" rel="noopener noreferrer">Paul Rosero</a>.</p>
+            </Row>
+        </Container>
+        //
+        //      
     )
 }
 
