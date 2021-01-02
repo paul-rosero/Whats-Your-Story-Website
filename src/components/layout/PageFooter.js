41e66bb9468b28eb0305b9f574b36ecfdd28b6ff
Paul Rosero <paul.rosero23@gmail.com>
2021-01-02 12:54:31 -0500

    remove code that belonged to react-mdl

--- src/components/layout/PageFooter.js
+++ src/components/layout/PageFooter.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { Footer, FooterSection, FooterLinkList } from "react-mdl";
 
 
 function PageFooter() {
     return (
-        <Footer size="mini" style={{ background: "rgb(40,43,130)", color: "white" }}>
-            <FooterSection type="left" logo="What's Your Story?">
-                <FooterLinkList>  
-                    <a href="/">Home</a>
-                    <p style={{margin: 0, padding: 0}}>This website was created By Paul Rosero.</p>
-                </FooterLinkList>
-            </FooterSection>
-        </Footer>
+        
+        // style={{ contentAlign: "center", background: "rgb(40,43,130)", color: "white" }}
+        //      <p>This website was created By <a href="https://paul-rosero-portfolio.herokuapp.com/" target="_blank" rel="noopener noreferrer">Paul Rosero</a>.</p>
     )
 }
 
