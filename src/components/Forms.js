096268c3e5a335ce9b5d71ea55cb5b3fcba1c081
Paul Rosero <paul.rosero23@gmail.com>
2021-01-02 11:18:15 -0500

    add code to be able to send emails

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,37 +1,38 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 
-// function sendEmail(e) {
-//     e.preventDefault();
+function sendEmail(e) {
+    e.preventDefault();
 
-//     emailjs.sendForm('gmail', 'template_z3d9ajg', e.target, 'user_EngDUayYS8iZ5uyXLsnzZ')
-//       .then((result) => {
-//           console.log(result.text);
-//       }, (error) => {
-//           console.log(error.text);
-//       });
-//       e.target.reset()
-// }
+    emailjs.sendForm('service_s98zskc', 'template_z3d9ajg', e.target, 'user_EngDUayYS8iZ5uyXLsnzZ')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+}
 
 export function EmailForm() {
     return (
-        <Form className="email-form">{/* onSubmit={ sendEmail }*/}  
+        <Form className="email-form" onSubmit={ sendEmail }>
             <Form.Group controlId="formBasicEmail">
                 <Form.Label />
-                <Form.Control type="text" placeholder="First Name" />
+                <Form.Control type="text" placeholder="First Name" name="first_name"/>
 
                 <Form.Label />
-                <Form.Control type="text" placeholder="Last Name" />
+                <Form.Control type="text" placeholder="Last Name" name="last_name"/>
 
                 <Form.Label />
-                <Form.Control type="text" placeholder="Subject" />
+                <Form.Control type="text" placeholder="Subject" name="subject"/>
 
                 <Form.Label />
-                <Form.Control type="email" placeholder="Email" />
+                <Form.Control type="email" placeholder="Email" name="email"/>
             </Form.Group>
             <Form.Group controlId="exampleForm.ControlTextarea1">
                 <Form.Label />
-                <Form.Control as="textarea" rows={3} placeholder="Type your message here..."/>
+                <Form.Control as="textarea" rows={3} placeholder="Type your message here..." name="message"/>
             </Form.Group>
             <Form.Group as={Row}>
                 <Col xs={{ span: 10, offset: 4 }} md={{ span: 10, offset: 5 }} >
