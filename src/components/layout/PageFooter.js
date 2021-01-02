61df60a00280fb009958e0a128cdce3d83cf22b8
Paul Rosero <paul.rosero23@gmail.com>
2021-01-02 13:34:29 -0500

    remove empty space

--- src/components/layout/PageFooter.js
+++ src/components/layout/PageFooter.js
@@ -7,7 +7,8 @@ function PageFooter() {
             <Row style={{ contentAlign: "center", background: "rgb(40,43,130)", color: "white", padding: "1vmax", justifyContent: "center" }}>
                 <p>&copy; {new Date().getFullYear()} Copyright: Created by <a style={{ fontSize: "2.5vh" }} href="https://paul-rosero-portfolio.herokuapp.com/" target="_blank" rel="noopener noreferrer">Paul Rosero</a>.</p>
             </Row>
-        </Container>
+        </Container>    
+    )
 }
 
 export default PageFooter;
