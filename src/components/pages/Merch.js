8333ed99445087e3902d2e8a961278a239ddf091
Paul Rosero <paul.rosero23@gmail.com>
2021-01-03 20:03:06 -0500

    add amazon website where the merch is located

--- src/components/pages/Merch.js
+++ src/components/pages/Merch.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+// const url = "https://www.amazon.com/s?rh=n%3A7141123011%2Cp_4%3AWhat%27s+Your+Story&ref=bl_sl_s_ap_web_7141123011"
+
 class Merch extends Component {
     render() {
         return (
